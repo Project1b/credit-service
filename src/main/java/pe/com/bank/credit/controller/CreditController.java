e0c23cb2f96f299d67fdf523579bbf94377143ba
leonel778 <104863395+leonel778@users.noreply.github.com>
2022-05-10 15:07:51 -0500

    tesAB

--- src/main/java/pe/com/bank/credit/controller/CreditController.java
+++ src/main/java/pe/com/bank/credit/controller/CreditController.java
@@ -39,7 +39,7 @@ public class CreditController {
                 .log();
     }
 
-//prueba de commit A
+//prueba de commit AB
     @PostMapping("/credits")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<CreditEntity> addCredit(@RequestBody CreditEntity creditEntity) {
