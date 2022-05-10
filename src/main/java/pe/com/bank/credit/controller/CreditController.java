a61d4aaa16e240e61b2854ed16035c7708488326
leonel778 <104863395+leonel778@users.noreply.github.com>
2022-05-10 14:58:38 -0500

    tes1234

--- src/main/java/pe/com/bank/credit/controller/CreditController.java
+++ src/main/java/pe/com/bank/credit/controller/CreditController.java
@@ -39,7 +39,7 @@ public class CreditController {
                 .log();
     }
 
-
+//prueba de commit
     @PostMapping("/credits")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<CreditEntity> addCredit(@RequestBody CreditEntity creditEntity) {
